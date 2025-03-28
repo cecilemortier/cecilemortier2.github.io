646121ce1aa12c9ba7308e04858ef710d3a93cfb
Gvico <42806519+Gvico@users.noreply.github.com>
2025-03-28 11:28:04 +0100

    Update kiné design

--- scripts/decode64.js
+++ scripts/decode64.js
@@ -1,4 +1,4 @@
 var encodedStr = "MDQ3MyA2NCA2NCAyNA==";
 var decodedStr = atob(encodedStr);
 
-document.getElementById('coded').innerHTML = decodedStr;
+document.getElementById('coded').innerHTML = '<a href="tel:' + decodedStr.replace(/\s+/g, '') + '">' + decodedStr + '</a>';
